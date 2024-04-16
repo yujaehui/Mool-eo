4bfd8da439d3d083cd2990f33c917bf7b20ba0a8
Jaehui Yu <pkj0522@naver.com>
2024-04-16 16:14:54 +0900

    [feat] JoinView 관련 로직 처리
    - 아이디, 비밀번호 유효성 검사
    - 아이디 중복확인 네트워크 통신

--- Mool-eo/Join/JoinViewController.swift
+++ Mool-eo/Join/JoinViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RxSwift
+import Toast
 
 class JoinViewController: BaseViewController {
     
@@ -23,5 +24,39 @@ class JoinViewController: BaseViewController {
     }
     
     override func bind() {
+        let id = joinView.idView.customTextField.rx.text.orEmpty.asObservable()
+        let password = joinView.passwordView.customTextField.rx.text.orEmpty.asObservable()
+        let idCheckButtonTap = joinView.idCheckButton.rx.tap.asObservable()
+        let nextButtonTap = joinView.nextButton.rx.tap.asObservable()
+        let input = JoinViewModel.Input(id: id, password: password, idCheckButtonTap: idCheckButtonTap, nextButtonTap: nextButtonTap)
+        
+        let output = viewModel.transform(input: input)
+        
+        output.idValidation.drive(with: self) { owner, value in
+            owner.joinView.idCheckButton.isEnabled = value
+            owner.joinView.idView.descriptionLabel.textColor = value ? ColorStyle.subText : ColorStyle.caution
+            owner.joinView.idView.descriptionLabel.text = TextFieldType.id.description
+        }.disposed(by: disposeBag)
+        
+        output.idCheckValidation.drive(with: self) { owner, value in
+            guard let value = value else { return }
+            owner.joinView.idView.descriptionLabel.textColor = value ? ColorStyle.subText : ColorStyle.caution
+        }.disposed(by: disposeBag)
+        
+        output.idCheckMessage.drive(with: self) { owner, value in
+            owner.joinView.idView.descriptionLabel.text = value.message
+        }.disposed(by: disposeBag)
+        
+        output.passwordValidation.drive(with: self) { owner, value in
+            owner.joinView.passwordView.descriptionLabel.textColor = value ? ColorStyle.subText : ColorStyle.caution
+        }.disposed(by: disposeBag)
+        
+        output.nextButtonValidation.drive(with: self) { owner, value in
+            owner.joinView.nextButton.isEnabled = value
+        }.disposed(by: disposeBag)
+        
+        output.nextButtonTap.bind(with: self) { owner, _ in
+            owner.navigationController?.pushViewController(JoinSecondViewController(), animated: true)
+        }.disposed(by: disposeBag)
     }
 }
--- Mool-eo/Join/JoinViewModel.swift
+++ Mool-eo/Join/JoinViewModel.swift
@@ -13,15 +13,87 @@ class JoinViewModel: ViewModelType {
     var disposeBag: DisposeBag = DisposeBag()
     
     struct Input {
-        
+        let id: Observable<String>
+        let password: Observable<String>
+        let idCheckButtonTap: Observable<Void>
+        let nextButtonTap: Observable<Void>
     }
     
     struct Output {
-        
+        let idValidation: Driver<Bool>
+        let idCheckValidation: Driver<Bool?>
+        let idCheckMessage: Driver<EmailModel>
+        let passwordValidation: Driver<Bool>
+        let nextButtonValidation: Driver<Bool>
+        let nextButtonTap: Observable<Void>
     }
     
     func transform(input: Input) -> Output {
-
-        return Output()
+        let idValidation = BehaviorSubject<Bool>(value: false)
+        
+        let idCheckValidation = BehaviorSubject<Bool?>(value: false)
+        let idCheckMessage = PublishSubject<EmailModel>()
+        
+        let passwordValidation = BehaviorSubject<Bool>(value: false)
+        
+        let nextButtonValidation = BehaviorSubject<Bool>(value: false)
+        
+        
+        input.id
+            .map { id in
+                let idRegex = "^(?=.*[a-z])[a-z0-9]{4,12}$"
+                let idPredicate = NSPredicate(format: "SELF MATCHES %@", idRegex)
+                return idPredicate.evaluate(with: id)
+            }
+            .debug("id")
+            .bind(with: self) { owner, value in
+                idValidation.onNext(value)
+                idCheckValidation.onNext(nil) // 중복 확인을 진행하지 않은 상태로 돌려놔야 하기 때문에
+            }.disposed(by: disposeBag)
+        
+        input.idCheckButtonTap
+            .withLatestFrom(input.id)
+            .map { id in
+                return EmailQuery(email: id)
+            }
+            .flatMap { query in
+                NetworkManager.emailCheck(query: query)
+            }
+            .debug("idCheck")
+            .subscribe(with: self) { owner, value in
+                if value.message == "사용 가능한 이메일입니다." {
+                    idCheckValidation.onNext(true)
+                } else {
+                    idCheckValidation.onNext(false)
+                }
+                idCheckMessage.onNext(value)
+            }.disposed(by: disposeBag)
+        
+        input.password
+            .map { password in
+                let passwordRegex = "^(?=.*[a-z])(?=.*\\d)[a-z\\d]{6,20}$"
+                let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordRegex)
+                return passwordPredicate.evaluate(with: password)
+            }
+            .debug("password")
+            .bind(to: passwordValidation)
+            .disposed(by: disposeBag)
+        
+        Observable.combineLatest(idCheckValidation, passwordValidation)
+            .map { (idCheck, password) in
+                guard let idCheck = idCheck else { return false } // idCheck가 nil, 중복 확인이 되지 않은 경우 false를 반환
+                return idCheck && password
+            }
+            .subscribe(with: self) { owner, value in
+                nextButtonValidation.onNext(value)
+            }.disposed(by: disposeBag)
+        
+        
+        return Output(idValidation: idValidation.asDriver(onErrorJustReturn: false),
+                      idCheckValidation: idCheckValidation.asDriver(onErrorJustReturn: nil),
+                      idCheckMessage: idCheckMessage.asDriver(onErrorJustReturn: EmailModel(message: "")),
+                      passwordValidation: passwordValidation.asDriver(onErrorJustReturn: false),
+                      nextButtonValidation: nextButtonValidation.asDriver(onErrorJustReturn: false),
+                      nextButtonTap: input.nextButtonTap)
     }
 }
